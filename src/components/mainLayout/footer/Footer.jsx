86d751692597e0c5c8508615d15198c1677c18ad
Muhammad Saqib <saqi@ip-192-168-1-123.us-west-2.compute.internal>
2021-09-21 12:36:51 -0400

    adds footer

---
+++ src/components/mainLayout/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './footer.css'
+export default function Footer() {
+    return (
+        <footer class="footer">
+            <div class="container">
+                <p>copyright 2021</p>
+            </div>
+        </footer>
+    )
+}
